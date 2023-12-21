465ad1fb19f9681926d97ff3e8610f261a7c415a
Trungg Sơnn <trungson.2209@gmail.com>
2023-12-21 21:56:36 +0700

    [Add] TSon Car

---
+++ src/main/java/com/vti/TSonCar/TSonCarApplication.java
@@ -0,0 +1,13 @@
+package com.vti.TSonCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TSonCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TSonCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/TSonCar/TSonCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.TSonCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TSonCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
