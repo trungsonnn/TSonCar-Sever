fdbc9b40c7575b2517d8801e6682eec43a3cfab8
Trungg Sơnn <trungson.2209@gmail.com>
2023-12-26 21:00:07 +0700

    [Add] Car Entity

--- src/main/java/com/vti/TSonCar/entity/Car.java
+++ src/main/java/com/vti/TSonCar/entity/Car.java
@@ -1,17 +1,46 @@
 package com.vti.TSonCar.entity;
 
 import jakarta.persistence.*;
-import lombok.Generated;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "car")
 public class Car {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "license_plate")
-    private String liscense_plate;
+
+    @EmbeddedId
+    private PrimaryKey pk;
+
+    @Column(name = "customer_name", length = 50, nullable = false)
+    private String customerName;
+
+    @Column(name = "catalogs", length = 50, nullable = false)
+    private String catalogs;
+
+    @Column(name = "car_maker", length = 50, nullable = false)
+    private String carMaker;
+
+    @OneToMany(mappedBy = "car")
+    private List<Accessory> accessories;
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode
+    @Embeddable
+    public static class PrimaryKey {
+        @Column(name = "license_plate", length = 10, nullable = false)
+        private String licensePlate;
+
+        @Column(name = "repair_date", nullable = false)
+        private LocalDate repairDate;
+    }
+
+
 }
