038fa51f2f849c6f1f147e7af8537b20d6b7031e
Trungg Sơnn <trungson.2209@gmail.com>
2023-12-26 20:59:43 +0700

    [Add] Car Dto

--- src/main/java/com/vti/TSonCar/dto/CarDto.java
+++ src/main/java/com/vti/TSonCar/dto/CarDto.java
@@ -1,2 +1,16 @@
-package com.vti.TSonCar.dto;public class CarDto {
+package com.vti.TSonCar.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class CarDto {
+    private String licensePlate;
+    private LocalDate repairDate;
+    private String customerName;
+    private String catalogs;
+    private String carMaker;
 }
