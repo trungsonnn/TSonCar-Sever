28b48cdad6aa194320579ad8a4f7cdf803489e64
Trungg Sơnn <trungson.2209@gmail.com>
2023-12-26 21:02:36 +0700

    [Add] Car Service

--- src/main/java/com/vti/TSonCar/service/CarService.java
+++ src/main/java/com/vti/TSonCar/service/CarService.java
@@ -1,2 +1,16 @@
-package com.vti.TSonCar.service;public class CarService {
+package com.vti.TSonCar.service;
+
+import com.vti.TSonCar.dto.CarDto;
+import com.vti.TSonCar.entity.Car;
+import com.vti.TSonCar.form.CarCreateForm;
+import com.vti.TSonCar.form.CarUpdateForm;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CarService {
+    Page<CarDto> findAll(Pageable pageable);
+    CarDto create(CarCreateForm form);
+    CarDto update(CarUpdateForm form);
+    void deteleById(Car.PrimaryKey pk);
+
 }
