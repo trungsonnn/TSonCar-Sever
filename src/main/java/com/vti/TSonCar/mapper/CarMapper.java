36bc4d7b3da9c9a8941c0cfdc9184a7e12ce8e04
Trungg Sơnn <trungson.2209@gmail.com>
2023-12-26 21:01:27 +0700

    [Add] Car Mapper

--- src/main/java/com/vti/TSonCar/mapper/CarMapper.java
+++ src/main/java/com/vti/TSonCar/mapper/CarMapper.java
@@ -1,2 +1,40 @@
-package com.vti.TSonCar.mapper;public class CarMapper {
-}
+package com.vti.TSonCar.mapper;
+
+import com.vti.TSonCar.dto.CarDto;
+import com.vti.TSonCar.entity.Car;
+import com.vti.TSonCar.form.CarCreateForm;
+import com.vti.TSonCar.form.CarUpdateForm;
+
+public class CarMapper {
+    public static Car map(CarCreateForm form) {
+        var car = new Car();
+        var pk = new Car.PrimaryKey();
+        pk.setLicensePlate(form.getLicensePlate());
+        pk.setRepairDate(form.getRepairDate());
+        car.setPk(pk);
+        car.setCustomerName(form.getCustomerName());
+        car.setCarMaker(form.getCarMaker());
+        car.setCatalogs(form.getCatalogs());
+        return car;
+    }
+
+    public static Car map(CarUpdateForm form) {
+        var car = new Car();
+        car.setCatalogs(form.getCatalog());
+        car.setCarMaker(form.getCarMaker());
+        car.setCustomerName(form.getCustomerName());
+        return car;
+    }
+
+    public static CarDto map(Car car) {
+        var dto = new CarDto();
+        var pk = car.getPk();
+        dto.setLicensePlate(pk.getLicensePlate());
+        dto.setRepairDate(pk.getRepairDate());
+        dto.setCustomerName(car.getCustomerName());
+        dto.setCatalogs(car.getCatalogs());
+        dto.setCarMaker(car.getCarMaker());
+        return dto;
+    }
+    }
+
