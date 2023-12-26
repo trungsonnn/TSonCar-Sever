b2ea7732cc97b59cd5489a6d477fdbe5ac08e100
Trungg Sơnn <trungson.2209@gmail.com>
2023-12-26 21:01:53 +0700

    [Add] Car Repository

--- src/main/java/com/vti/TSonCar/repository/CarRepository.java
+++ src/main/java/com/vti/TSonCar/repository/CarRepository.java
@@ -1,2 +1,10 @@
-package com.vti.TSonCar.repository;public interface CarRepository {
+package com.vti.TSonCar.repository;
+
+import com.vti.TSonCar.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+
+public interface CarRepository extends JpaRepository<Car,Car.PrimaryKey>, JpaSpecificationExecutor<Car> {
 }
