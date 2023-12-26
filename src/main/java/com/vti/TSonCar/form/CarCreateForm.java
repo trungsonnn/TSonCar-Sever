fe6babf15a6d4d0e58a333d5bce0115c24c258fb
Trungg Sơnn <trungson.2209@gmail.com>
2023-12-26 21:00:33 +0700

    [Add] Car Create Form

--- src/main/java/com/vti/TSonCar/form/CarCreateForm.java
+++ src/main/java/com/vti/TSonCar/form/CarCreateForm.java
@@ -1,2 +1,34 @@
-package com.vti.TSonCar.form;public class CarCreateForm {
+package com.vti.TSonCar.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class CarCreateForm {
+    @NotBlank(message = "{car.licensePlate.NotBlank.message}")
+    @Length(max = 10, message = "{car.licensePlate.Length.message}")
+    private String licensePlate;
+
+    @NotNull
+    @PastOrPresent
+    private LocalDate repairDate;
+
+    @NotBlank
+    @Length(max = 50)
+    private String customerName;
+
+    @NotBlank
+    @Length(max = 50)
+    private String catalogs;
+
+    @NotBlank
+    @Length(max = 50)
+    private String carMaker;
 }
